2d13a5c564bddec62ca052b7b77f190a48c44c72
gmx <mingxingguan@yeah.net>
2023-06-30 14:51:21 +0800

    add vector instruction rename table(freelist/rat/rollbacklist) and parameters

---
+++ src/main/scala/xiangshan/vector/virename/VIFreeList.scala
@@ -0,0 +1,105 @@
+/***************************************************************************************
+ * Copyright (c) 2020-2023 Institute of Computing Technology, Chinese Academy of Sciences
+ *
+ * XiangShan is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
+
+
+/*--------------------------------------------------------------------------------------
+    Author: GMX
+    Date: 2023-06-28
+    email: guanmingxing@bosc.ac.cn
+
+    Free List for allocating free physical reg id
+---------------------------------------------------------------------------------------*/
+
+package xiangshan.vector.virename
+
+import chisel3._
+import chisel3.util._
+import xiangshan._
+import utils._
+import xs.utils.{CircularQueuePtr, HasCircularQueuePtrHelper, CircularShift}
+import xiangshan.vector.virename._
+import chipsalliance.rocketchip.config._
+
+class VIFreeListBundle(implicit p: Parameters) extends VIRenameBundle {
+
+    def reqNumWidth: Int = log2Up(VIRenameWidth + 1)
+
+    //be different from int and float rename module
+    //freeEntry num, determines whether the requests can enter RenameModule
+
+    val canAllocateNum = Output(UInt(reqNumWidth.W))
+    val doAllocate = Input(Bool())
+
+    //connects with RAT, offer PhyRegIndex
+    val allocateReqNum = Input(UInt(reqNumWidth.W))
+    val allocatePhyReg = Output(Vec(VIRenameWidth, UInt(VIPhyRegIdxWidth.W)))
+
+    //connects with RollBackList
+    val releaseReqMask = Input(Vec(VICommitWidth, Bool()))
+    val releaseReqPhyReg = Input(Vec(VICommitWidth, UInt(VIPhyRegIdxWidth.W)))
+}
+
+class VIFreeList(implicit p: Parameters) extends VIRenameModule with HasCircularQueuePtrHelper {
+    val io = IO(new VIFreeListBundle)
+
+    class VIFreeListPtr extends CircularQueuePtr[VIFreeListPtr](VIPhyRegsNum)
+    object VIFreeListPtr {
+        def apply(f: Boolean, v: Int): VIFreeListPtr = {
+            val ptr = Wire(new VIFreeListPtr)
+            ptr.flag := f.B
+            ptr.value := v.U
+            ptr
+        }
+    }
+
+    //free list
+    val freeList_ds = VecInit(Seq.tabulate(VIPhyRegsNum)(i => i.U(VIPhyRegIdxWidth.W)))
+    val freeList = RegInit(freeList_ds)
+
+    //head and tail pointer
+    val headPtr = RegInit(VIFreeListPtr(false, 0))
+    val tailPtr = RegInit(VIFreeListPtr(false, VIPhyRegsNum - 1))
+
+    //Allocate
+    val freeEntryNum = Wire(UInt(VIPhyRegIdxWidth.W))
+    freeEntryNum := distanceBetween(tailPtr, headPtr) + 1.U
+
+    io.canAllocateNum := Mux(freeEntryNum >= VIRenameWidth.U, VIRenameWidth.U, freeEntryNum)
+
+    //val allocateNum = io.allocateReqNum
+    val headPtrNext = headPtr + io.allocateReqNum
+    headPtr := Mux(io.doAllocate, headPtrNext, headPtr)
+
+    val headPtrOHVec = Wire(Vec(VIRenameWidth, UInt(VIPhyRegsNum.W)))
+    for(i <- 0 until VIRenameWidth) {
+        headPtrOHVec(i) := (headPtr + i.U).toOH
+    }
+
+    val phyRegCandidates = Wire(Vec(VIRenameWidth, UInt(VIPhyRegIdxWidth.W)))
+    phyRegCandidates := headPtrOHVec.map(sel => Mux1H(sel, freeList))
+    io.allocatePhyReg := phyRegCandidates
+
+    //Release
+    val releaseNum = PopCount(io.releaseReqMask)
+    val tailPtrNext = tailPtr + releaseNum
+    tailPtr := tailPtrNext
+
+    for(i <- 0 until VICommitWidth) {
+        val releasePtr = tailPtr + i.U
+        when(io.releaseReqMask(i) === 1.U) {
+            freeList(releasePtr.value) := io.releaseReqPhyReg(i)
+        }
+    }
+}
---
+++ src/main/scala/xiangshan/vector/virename/VIRobIdxQueue.scala
@@ -0,0 +1,139 @@
+/***************************************************************************************
+ * Copyright (c) 2020-2023 Institute of Computing Technology, Chinese Academy of Sciences
+ *
+ * XiangShan is licensed under Mulan PSL v2.
+ * You can use this software according to the terms and conditions of the Mulan PSL v2.
+ * You may obtain a copy of Mulan PSL v2 at:
+ *          http://license.coscl.org.cn/MulanPSL2
+ *
+ * THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+ * EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+ * MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+ *
+ * See the Mulan PSL v2 for more details.
+ ***************************************************************************************/
+
+
+/*--------------------------------------------------------------------------------------
+    Author: GMX
+    Date: 2023-06-28
+    email: guanmingxing@bosc.ac.cn
+
+---------------------------------------------------------------------------------------*/
+
+package xiangshan.vector.virename
+
+import chisel3._
+import chisel3.util._
+import chipsalliance.rocketchip.config.Parameters
+import xs.utils.{CircularQueuePtr, HasCircularQueuePtrHelper, CircularShift}
+
+import xiangshan._
+import utils._
+
+class VIRobIdxQueueDeqIO(implicit p: Parameters) extends VIRenameBundle {
+    val hasPendingRobIdx = Input(Bool())
+    val pendingType = Input(Bool()) //0-commit, 1-walk
+
+    val canCommitRobIdxNum = Input(UInt(log2Up(VICommitWidth + 1).W))
+    val canRollBackRobIdxNum = Input(UInt(log2Up(VICommitWidth + 1).W))
+
+    val doCommit = Output(Bool())
+    val doWalk = Output(Bool())
+    val robIdx = Output(Vec(VICommitWidth, UInt(log2Up(RobSize).W)))
+    val mask = Output(Vec(VICommitWidth, Bool()))
+
+    val hasWalk = Output(Bool())
+}
+
+class VIRobIdxQueueEnqIO(implicit p: Parameters) extends VIRenameBundle {
+    val canEnq = Output(Bool())
+    val doCommit = Input(Bool())
+    val doWalk = Input(Bool())
+    val mask = Input(Vec(VICommitWidth, Bool()))
+    val robIdx = Input(Vec(VICommitWidth, UInt(log2Up(RobSize).W)))
+}
+
+class VICommitReqEntry(implicit p: Parameters) extends VIRenameBundle {
+    val robIdx = UInt(log2Up(RobSize).W)
+    val commitType = Bool() //0-commit, 1-rollback
+}
+
+class VIRobIdxQueue(size: Int)(implicit p: Parameters) extends VIRenameModule with HasCircularQueuePtrHelper {
+    val io = IO(new Bundle {
+        val out = new VIRobIdxQueueDeqIO
+        val in = new VIRobIdxQueueEnqIO
+    })
+
+    class robIdxQueuePtr extends CircularQueuePtr[robIdxQueuePtr](size)
+    object robIdxQueuePtr {
+        def apply(f: Boolean, v: Int): robIdxQueuePtr = {
+            val ptr = Wire(new robIdxQueuePtr)
+            ptr.flag    := f.B
+            ptr.value   := v.U
+            ptr
+        }
+    }
+
+    val robIdxQueue_ds = Vec(size, new VICommitReqEntry)
+    val robIdxQueue = Reg(robIdxQueue_ds)
+
+    val walkNum = RegInit(UInt(log2Up(VIPhyRegsNum + 1).W), 0.U)
+
+    val headPtr = RegInit(robIdxQueuePtr(false, 0))
+    val tailPtr = RegInit(robIdxQueuePtr(false, 0))
+
+    val freeEntryNum = distanceBetween(tailPtr, headPtr)
+
+    //enq
+    io.in.canEnq := freeEntryNum >= VICommitWidth.U
+    val enqNum = PopCount(io.in.mask)
+
+    val entryEnq = io.in.doCommit || io.in.doWalk
+
+    val headPtrNext = RegNext(headPtr + enqNum)
+    headPtr := Mux(entryEnq, headPtrNext, headPtr)
+
+    walkNum := Mux(io.in.doWalk, walkNum + enqNum, walkNum)
+    io.out.hasWalk := (walkNum === 0.U)
+
+    val enqType = Mux(io.in.doCommit, false.B, true.B)
+    for((e, i) <- io.in.robIdx.zipWithIndex) {
+        val wPtr = headPtr + i.U
+        when(io.in.mask(i) === true.B && entryEnq) {
+            robIdxQueue(wPtr.value).robIdx := e
+            robIdxQueue(wPtr.value).commitType := enqType
+        }
+    }
+
+    //deq
+    val deqTypeEqualVec = Wire(Vec(VICommitWidth, Bool()))
+    deqTypeEqualVec(0) := true.B
+    for(i <- 1 until VICommitWidth) {
+        deqTypeEqualVec(i) := deqTypeEqualVec(i-1) & (robIdxQueue(headPtr.value).commitType === robIdxQueue((headPtr + i.U).value).commitType) & (freeEntryNum >= i.U)
+    }
+    val deqTypeEqualNum = PopCount(deqTypeEqualVec)
+
+    val hasPendingIdx = io.out.hasPendingRobIdx
+    val hasPendingType = io.out.pendingType
+
+    val deqType = robIdxQueue(tailPtr.value).commitType
+
+    val deqNum = Wire(UInt(log2Up(VICommitWidth + 1).W))
+    deqNum := Mux(deqType === false.B, Mux(io.out.canRollBackRobIdxNum > deqTypeEqualNum, deqTypeEqualNum, io.out.canRollBackRobIdxNum), 
+                                            Mux(io.out.canCommitRobIdxNum > deqTypeEqualNum, deqTypeEqualNum, io.out.canCommitRobIdxNum))
+    val deqNumReal = Mux(io.out.hasPendingRobIdx && (io.out.pendingType =/= robIdxQueue(tailPtr.value).commitType), 0.U, deqNum)
+    for(i <- 0 until VICommitWidth) {
+        io.out.mask(i) := i.U < deqNum
+        when(deqNum > i.U) {
+            io.out.robIdx(i) := robIdxQueue(i).commitType
+        }.otherwise {
+            io.out.robIdx(i) := DontCare
+        }
+    }
+    io.out.doCommit := Mux((deqNum =/= 0.U) && (deqType === false.B), true.B, false.B)
+    io.out.doWalk := Mux((deqNum =/= 0.U) && (deqType === true.B), true.B, false.B)
+
+    val tailPtrNext = RegNext(tailPtr + deqNum)
+    tailPtr := Mux(deqNum =/= 0.U, tailPtrNext, tailPtr)
+}
