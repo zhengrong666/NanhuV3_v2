c234b59b5e2f39feaa266c5234a911283ffaae23
gmx <mingxingguan@yeah.net>
2023-07-04 16:48:21 +0800

    transfer only, dirty code

--- src/main/scala/xiangshan/vector/VectorBundle.scala
+++ src/main/scala/xiangshan/vector/VectorBundle.scala
@@ -27,33 +27,33 @@ import chisel3._
 import chisel3.util._
 import chipsalliance.rocketchip.config.Parameters
 
-import xiangshan.CtrlFlow
 import utils._
 
+import _root_.Vector._
 import _root_.Vector.videcode.{ImmUnion, VectorArithDecode}
-import Vector._
+import _root_.xiangshan.CtrlFlow
 
 //TODO: Vector Micro OP interface align
-//fake interface
-class VectorMicroOP(implicit p: Parameters) extends VectorBaseBundle {
-    def typeJudge(sel_type: UInt): Bool = sel_type === 1.U
-}
+// //fake interface
+// class VectorMicroOP(implicit p: Parameters) extends VectorBaseBundle {
+//     def typeJudge(sel_type: UInt): Bool = sel_type === 1.U
+// }
 
-class VIRenameReq(implicit p: Parameters) extends VectorBaseBundle {
-    val lvs1    = UInt(5.W)
-    val lvs2    = UInt(5.W)
-    val lvd     = UInt(5.W)
-    //TODO: WaitQueue port aligning...
-    val en      = Bool()
-}
+// class VIRenameReq(implicit p: Parameters) extends VectorBaseBundle {
+//     val lvs1    = UInt(5.W)
+//     val lvs2    = UInt(5.W)
+//     val lvd     = UInt(5.W)
+//     //TODO: WaitQueue port aligning...
+//     val en      = Bool()
+// }
 
-class VIWaitQueueToRenameBundle(implicit p: Parameters) extends VectorBaseBundle {
-    val renameReq   = Input(Vec(VIRenameWidth, new VIRenameReq))
-    val robIdx      = Input(Vec(VIRenameWidth, UInt(log2Up(RobSize).W)))
-    val needRename = Input(Vec(VIRenameWidth, Bool()))
-    val vtypeValue  = Input(UInt(64.W))
-    val doRename = Output(Bool())
-}
+// class VIWaitQueueToRenameBundle(implicit p: Parameters) extends VectorBaseBundle {
+//     val renameReq   = Input(Vec(VIRenameWidth, new VIRenameReq))
+//     val robIdx      = Input(Vec(VIRenameWidth, UInt(log2Up(RobSize).W)))
+//     val needRename = Input(Vec(VIRenameWidth, Bool()))
+//     val vtypeValue  = Input(UInt(64.W))
+//     val doRename = Output(Bool())
+// }
 
 class VICtrlSignals(implicit p: Parameters) extends VectorBaseBundle {
     val srcType = Vec(3, SrcType())
@@ -89,7 +89,6 @@ class VICtrlSignals(implicit p: Parameters) extends VectorBaseBundle {
         commitType := DontCare
         this
     }
-
 }
 
 class VICtrlFlow(implicit p: Parameters) extends VectorBaseBundle {
@@ -127,4 +126,15 @@ class VICtrl(implicit p: Parameters) extends VectorBaseBundle {
     val vd = UInt(128.W)
     val vxsat = UInt(1.W)
     val fflags = UInt(5.W)
+}
+
+//gmx: Vector Rename input signals, and it will transform to Vector MicroOP
+class VectorCtrlFlow(implicit p: Parameters) extends VectorBaseBundle {
+    val vCtrlSignals = new VICtrlSignals
+    val vCsrInfo = new VICsrInfo
+}
+
+class VectorMicroOP(implicit p: Parameters) extends VectorCtrlFlow {
+    val psrc = Vec(3, UInt(VIPhyRegIdxWidth.W))
+    val pdest = UInt(VIPhyRegIdxWidth.W)
 }
