956f74493241fda7133bde9c46482bf3fb8cb71f
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-08-02 18:27:57 +0800

    Update vector regfile

--- src/main/scala/xiangshan/vector/vbackend/vregfile/VRegfile.scala
+++ src/main/scala/xiangshan/vector/vbackend/vregfile/VRegfile.scala
@@ -2,16 +2,17 @@ package xiangshan.vector.vbackend.vregfile
 import chipsalliance.rocketchip.config.Parameters
 import chisel3._
 import chisel3.util._
-import xiangshan.{ExuOutput, XSBundle, XSModule}
+import xiangshan.vector.vbackend.vexecute.vfu.div.ZeroExt
+import xiangshan.{ExuOutput, Redirect, XSBundle, XSModule}
 
 class MoveReq(implicit p: Parameters) extends XSBundle{
   private val VRFSize = coreParams.vectorParameters.vPhyRegsNum
   val srcAddr = UInt(log2Up(VRFSize).W)
   val dstAddr = UInt(log2Up(VRFSize).W)
-  val ma = Bool()
-  val ta = Bool()
-  val tailMask = UInt((VLEN/8).W)
-  val prestartMask = UInt((VLEN/8).W)
+  val agnostic = Bool()
+  val enable = Bool()
+  val sew = UInt(2.W)
+  val uopIdx = UInt(7.W)
 }
 
 class VrfReadPort(implicit p: Parameters) extends XSBundle{
@@ -20,34 +21,24 @@ class VrfReadPort(implicit p: Parameters) extends XSBundle{
   val data = Output(UInt(VLEN.W))
 }
 
-class VrfWriteSignals(implicit p: Parameters) extends XSBundle{
-  val addr = UInt(PhyRegIdxWidth.W)
-  val data = Vec(VLEN / 8, UInt(8.W))
-  val mask = UInt((VLEN / 8).W)
-}
-
-class MrfWriteSignals(implicit p: Parameters) extends XSBundle{
-  val addr = UInt(PhyRegIdxWidth.W)
-  val data = UInt((VLEN / 8).W)
-}
-
 class VRegfile(wbWkpNum:Int, wbNoWkpNum:Int, readPortNum:Int)(implicit p: Parameters) extends XSModule {
   private val size = coreParams.vectorParameters.vPhyRegsNum
   private val dataWidth = VLEN
   private val maskWidth = VLEN / 8
   val io = IO(new Bundle{
-    val loadWb = Input(Vec(loadUnitNum, Valid(new ExuOutput)))
     val wbWakeup = Input(Vec(wbWkpNum, Valid(new ExuOutput)))
     val wbNoWakeup = Input(Vec(wbNoWkpNum, Valid(new ExuOutput)))
     val wakeups = Output(Vec(wbWkpNum, Valid(new ExuOutput)))
     val moveOldValReqs = Input(Vec(loadUnitNum, Valid(new MoveReq)))
     val readPorts = Vec(readPortNum, new VrfReadPort)
     val clear = Vec(vectorParameters.vRenameWidth, Flipped(ValidIO(UInt(PhyRegIdxWidth.W))))
+    val redirect = Input(Valid(new Redirect))
   })
   
   
-  private val mrf = Mem(size, UInt(maskWidth.W))
+  private val mrf = Mem(size, Vec(maskWidth, Bool()))
   private val vrf = Mem(size, Vec(maskWidth, UInt(8.W)))
+  private val fullMaskVec = VecInit(Seq.fill(maskWidth)(true.B))
 
   // read vector register file
   for (r <- io.readPorts) {
@@ -57,15 +48,18 @@ class VRegfile(wbWkpNum:Int, wbNoWkpNum:Int, readPortNum:Int)(implicit p: Parame
   for (i <- 0 until wbWkpNum) {
     val addr = io.wbWakeup(i).bits.uop.pdest
     val data = io.wbWakeup(i).bits.data.asTypeOf(Vec(dataWidth / 8, UInt(8.W)))
-    val dataMask = io.wbWakeup(i).bits.writeDataMask.asBools
-    val wkpMask = mrf(addr) | io.wbWakeup(i).bits.writeDataMask
+    val wbMask = io.wbWakeup(i).bits.writeDataMask.asBools
+    val wkpMask = io.wbWakeup(i).bits.wakeupMask.asBools
     when (io.wbWakeup(i).valid) {
-      vrf.write(addr, data, dataMask)
-      mrf(addr) := Mux(wkpMask.andR, 0.U, wkpMask)
+      vrf.write(addr, data, wbMask)
+      mrf.write(addr, fullMaskVec, wkpMask)
     }
     // wakeup
-    io.wakeups(i).valid := io.wbWakeup(i).valid && wkpMask.andR
-    io.wakeups(i).bits := io.wbWakeup(i).bits
+    val wbValidReg = RegNext(io.wbWakeup(i).valid, false.B)
+    val wbBitsReg = RegEnable(io.wakeups(i).bits, io.wbWakeup(i).valid)
+    val maskRead = mrf.read(wbBitsReg.uop.pdest)
+    io.wakeups(i).valid := wbValidReg && !wbBitsReg.uop.robIdx.needFlush(io.redirect) && maskRead.reduce(_&_)
+    io.wakeups(i).bits := wbBitsReg
   }
   for(c <- io.clear){
     when(c.valid){
@@ -87,20 +81,22 @@ class VRegfile(wbWkpNum:Int, wbNoWkpNum:Int, readPortNum:Int)(implicit p: Parame
     when (mReq.valid) {
       val dst = io.moveOldValReqs(i).bits.dstAddr
       val srcData = WireInit(vrf.read(mReq.bits.srcAddr))
-      val dstMask = mrf(io.moveOldValReqs(i).bits.dstAddr)
-      val ma = mReq.bits.ma
-      val ta = mReq.bits.ta
-      val tm = mReq.bits.tailMask
-      val mm = (~dstMask).asUInt | (~mReq.bits.tailMask).asUInt | (~mReq.bits.prestartMask).asUInt
-      for((d,i) <- srcData.zipWithIndex){
-        when(ta && tm(i) || ma && mm(i)){
-          d := ((1 << d.getWidth ) - 1).U
-        }
+      val elmIdx = MuxCase(0.U, Seq(
+        mReq.bits.sew === 0.U -> ZeroExt(mReq.bits.uopIdx(log2Ceil(VLEN / 8) - 1, 0), log2Ceil(VLEN / 8)),
+        mReq.bits.sew === 1.U -> ZeroExt(mReq.bits.uopIdx(log2Ceil(VLEN / 16) - 1, 0), log2Ceil(VLEN / 8)),
+        mReq.bits.sew === 2.U -> ZeroExt(mReq.bits.uopIdx(log2Ceil(VLEN / 32) - 1, 0), log2Ceil(VLEN / 8)),
+        mReq.bits.sew === 3.U -> ZeroExt(mReq.bits.uopIdx(log2Ceil(VLEN / 64) - 1, 0), log2Ceil(VLEN / 8)),
+      ))
+      val wm = MuxCase(0.U, Seq(
+        mReq.bits.sew === 0.U -> (0x1.U << elmIdx)(maskWidth - 1, 0),
+        mReq.bits.sew === 1.U -> (0x3.U << elmIdx)(maskWidth - 1, 0),
+        mReq.bits.sew === 2.U -> (0xf.U << elmIdx)(maskWidth - 1, 0),
+        mReq.bits.sew === 3.U -> (0xff.U << elmIdx)(maskWidth - 1, 0),
+      ))
+      when(mReq.bits.agnostic && !mReq.bits.enable){
+        srcData.foreach(_ := 0xff.U)
       }
-      val wm = (~dstMask).asUInt | mReq.bits.tailMask | mReq.bits.prestartMask
-      val new_mask = dstMask | mReq.bits.tailMask | mReq.bits.prestartMask
       vrf.write(dst, srcData, wm.asBools)
-      mrf.write(dst, new_mask)
     }
   }
 }
