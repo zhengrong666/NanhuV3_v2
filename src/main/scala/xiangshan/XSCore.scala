386d9595fbf28bf19bd17cd00c0e228bc5710e80
zhang bin <zhangbin@open02.boscszcluster.com>
2023-08-25 16:25:53 +0800

    update the io between csr and ctrlblock

--- src/main/scala/xiangshan/XSCore.scala
+++ src/main/scala/xiangshan/XSCore.scala
@@ -133,7 +133,8 @@ class XSCoreImp(outer: XSCoreBase) extends LazyModuleImp(outer)
 
   csrioIn.vcsr := DontCare
   csrioIn.vcsr.robWb := DontCare
-  ctrlBlock.io.vtypeWb := csrioIn.vcsr.vtype.vtypeWbToRename
+  ctrlBlock.io.vcsrio := DontCare
+  ctrlBlock.io.vcsrio.vtypeWbToRename := csrioIn.vcsr.vtype.vtypeWbToRename
   
   
   
