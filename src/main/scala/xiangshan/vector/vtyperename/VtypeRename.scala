65ce667ff1eb106bf986e80b57f1c867e3784801
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-08-26 10:48:26 +0800

    Update VtypeRename redirect logics and first entry initialization.

--- src/main/scala/xiangshan/vector/vtyperename/VtypeRename.scala
+++ src/main/scala/xiangshan/vector/vtyperename/VtypeRename.scala
@@ -57,29 +57,18 @@ class VTypeRenameTable(size:Int)(implicit p: Parameters) extends XSModule{
     val redirect = Input(Valid(new Redirect))
     val flushVec = Output(UInt(size.W))
   })
-  private val table = Reg(Vec(size, new VTypeEntry))
-  private val doReset = RegInit(true.B)
+  private val headEntry = RegInit(0.U.asTypeOf(new VTypeEntry))
+  private val tailEntries = Seq.fill(size - 1)(Reg(new VTypeEntry))
+  private val table = headEntry +: tailEntries
 
   for((entry, idx) <- table.zipWithIndex){
     val hitSeq = io.w.map(w => w.en && w.addr === idx.U)
     val dataSeq = io.w.map(_.data)
     val data = Mux1H(hitSeq, dataSeq)
     val en = hitSeq.reduce(_|_)
-    if(idx == 0){
-      when(doReset) {
-        entry := 0.U.asTypeOf(new VTypeEntry)
-      }.elsewhen(en){
-        entry := data
-      }
-    } else {
-      when(en) {
-        entry := data
-      }
+    when(en) {
+      entry := data
     }
-  }
-
-  when(doReset){
-    doReset := false.B
   }
 
   for(r <- io.r){
@@ -109,17 +98,20 @@ class VtypeRename(implicit p: Parameters) extends VectorBaseModule with HasCircu
   private val table = Module(new VTypeRenameTable(VIVtypeRegsNum))
   private val enqPtr = RegInit(enqPtrInit)
   private val deqPtr = RegInit(0.U.asTypeOf(new VtypePtr))
-  assert(enqPtr > deqPtr)
+  private val flushHeadPtr = RegInit(enqPtrInit)
+  assert((deqPtr + 1.U) === flushHeadPtr)
+  assert(enqPtr >= flushHeadPtr)
 
   class VtypePtr extends CircularQueuePtr[VtypePtr](VIVtypeRegsNum)
 
   private val validEntriesNum = distanceBetween(enqPtr, deqPtr)
   private val emptyEntiresNum = VIVtypeRegsNum.U - validEntriesNum
+
   private val enqMask = UIntToMask(enqPtr.value, VIVtypeRegsNum)
-  private val deqMask = UIntToMask(deqPtr.value, VIVtypeRegsNum)
-  private val enqXorDeq = enqMask ^ deqMask
-  private val validsMask = Mux(deqPtr.value <= enqPtr.value, enqXorDeq, (~enqXorDeq).asUInt)
-  private val redirectMask = validsMask & table.io.flushVec
+  private val flushHeadMask = UIntToMask(flushHeadPtr.value, VIVtypeRegsNum)
+  private val enqXorFlush = enqMask ^ flushHeadMask
+  private val maybeFlushMask = Mux(flushHeadPtr.value <= enqPtr.value, enqXorFlush, (~enqXorFlush).asUInt)
+  private val redirectMask = maybeFlushMask & table.io.flushVec
   private val flushNum = PopCount(redirectMask)
 
   private val setVlSeq = io.in.map(i => i.valid && i.bits.ctrl.isVtype)
@@ -243,6 +235,7 @@ class VtypeRename(implicit p: Parameters) extends VectorBaseModule with HasCircu
   private val comNumReg = RegEnable(commmitNum, setVlCommitted)
   when(comValidReg){
     deqPtr := deqPtr + comNumReg
+    flushHeadPtr := flushHeadPtr + comNumReg
   }
 
 
