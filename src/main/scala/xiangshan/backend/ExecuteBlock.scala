5b9988e9fb59c3864b981e186a4db9e7f8c14f82
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-08-27 16:55:30 +0800

    Fixed wiring errors

--- src/main/scala/xiangshan/backend/ExecuteBlock.scala
+++ src/main/scala/xiangshan/backend/ExecuteBlock.scala
@@ -171,7 +171,7 @@ class ExecuteBlock(val parentName:String = "Unknown")(implicit p:Parameters) ext
     fpBlk.io.csr_frm := intBlk.io.csrio.fpu.frm
 
     memRs.io.redirect := Pipe(localRedirect)
-    memRs.io.aluSpecWakeup.zip(intRs.io.aluSpecWakeup).foreach({case(a, b) => a := b})
+    memRs.io.aluJmpSpecWakeup.zip(intRs.io.aluJmpSpecWakeup).foreach({case(a, b) => a := b})
     memRs.io.mulSpecWakeup.zip(intRs.io.mulSpecWakeup).foreach({case(a, b) => a := b})
     memRs.io.earlyWakeUpCancel := memBlk.io.earlyWakeUpCancel(2)
     memRs.io.integerAllocPregs := io.integerAllocPregs
