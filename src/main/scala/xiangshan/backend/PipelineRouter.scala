06b984cc0db851e7d15b137a215402c0b00b15e9
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-08-28 21:36:44 +0800

    Simplified PipelineRouter

--- src/main/scala/xiangshan/backend/PipelineRouter.scala
+++ src/main/scala/xiangshan/backend/PipelineRouter.scala
@@ -10,52 +10,41 @@ class PipelineRouter[T <: Data](gen:T, vecLen:Int, outNum:Int) extends Module{
     val out = Vec(outNum, Vec(vecLen, Decoupled(gen)))
     val flush = Input(Bool())
   })
+  io.out.foreach(o => assert(PopCount(o.map(_.ready)) === 0.U || PopCount(o.map(_.ready)) === vecLen.U))
 
   private val validRegs = Seq.tabulate(outNum, vecLen)((_,_) => RegInit(false.B))
   private val bitsRegs = Seq.tabulate(outNum, vecLen)((_,_) => Reg(gen))
-  private val allowOuts = Seq.tabulate(outNum, vecLen)((_,_) => Wire(Bool()))
-  private val allowIns = Wire(Vec(vecLen,Bool()))
-  private val auxValids = RegInit(VecInit(Seq.tabulate(vecLen)(_ => false.B)))
+  private val masterValidReg = RegInit(false.B)
+  private val outReady = io.out.map(_.head.ready).reduce(_&_)
+  private val allowIn = (!masterValidReg || outReady) && !io.flush
 
-    for((sl, oidx) <- allowOuts.zipWithIndex) {
-    val otherReadies = io.out.zipWithIndex.filterNot(_._2 == oidx).map(_._1).map(_.map(_.ready)).transpose
-    for((s, rl) <- sl.zip(otherReadies)){
-      s := rl.reduce(_&_)
-    }
+  io.in.foreach(_.ready := allowIn)
+
+  private val inValid = io.in.map(_.valid).reduce(_|_)
+  when(io.flush){
+    masterValidReg := false.B
+  }.elsewhen(allowIn){
+    masterValidReg := inValid
   }
 
-
-  for((vs, bs) <- validRegs.zip(bitsRegs)){
-    for((((i, v), b), en) <- io.in.zip(vs).zip(bs).zip(allowIns)){
-      when(en){
-        v := i.valid
+  validRegs.zip(bitsRegs).foreach({case(vrl, brl) =>
+    vrl.zip(brl).zip(io.in).foreach({case((vr, br), in) =>
+      when(io.flush){
+        vr := false.B
+      }.elsewhen(allowIn) {
+        vr := in.valid
       }
-      when(i.fire){
-        b := i.bits
+      when(in.fire){
+        br := in.bits
       }
-      i.ready := en
-    }
-  }
-
-  for(((v, en), in) <- auxValids.zip(allowIns).zip(io.in)){
-    when(en){
-      v := in.valid
-    }
-  }
-
-  for ((((ol, vl), bl), el) <- io.out.zip(validRegs).zip(bitsRegs).zip(allowOuts)) {
-    for ((((o, v), b), e) <- ol.zip(vl).zip(bl).zip(el)) {
-      o.valid := v && !io.flush && e
-      o.bits := b
-    }
-  }
-
-  for((r, iidx) <- allowIns.zipWithIndex){
-    val fires = io.out.map(_(iidx)).map(_.fire)
-    r := fires.reduce(_&_) || auxValids(iidx) === false.B || io.flush
-  }
-
-  validRegs.foreach(vl => {
-    vl.zip(auxValids).foreach({case(a, b) => assert(a === b)})
+    })
   })
+
+  validRegs.zip(bitsRegs).zip(io.out).foreach({case((vrl, brl), ol) =>
+    vrl.zip(brl).zip(ol).foreach({case((vr, br), out) =>
+      out.valid := vr && !io.flush
+      out.bits := br
+    })
+  })
+
 }
