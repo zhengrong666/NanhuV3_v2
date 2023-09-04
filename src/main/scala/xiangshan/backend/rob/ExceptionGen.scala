a50e6bdbc02f0b2a8c42a6c25f66158b23c954f8
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-09-04 18:49:02 +0800

    Optimized exception generator timing

--- src/main/scala/xiangshan/backend/rob/ExceptionGen.scala
+++ src/main/scala/xiangshan/backend/rob/ExceptionGen.scala
@@ -22,8 +22,9 @@ package xiangshan.backend.rob
 import chipsalliance.rocketchip.config.Parameters
 import chisel3.util._
 import chisel3._
+import xiangshan.backend.issue.SelectPolicy
 import xiangshan.{ExceptionVec, Redirect, TriggerCf, XSBundle, XSModule}
-import xs.utils.{HasCircularQueuePtrHelper, ParallelOperation, ParallelPriorityMux}
+import xs.utils.{HasCircularQueuePtrHelper, ParallelPriorityMux}
 
 class RobExceptionInfo(implicit p: Parameters) extends XSBundle {
   // val valid = Bool()
@@ -43,30 +44,6 @@ class RobExceptionInfo(implicit p: Parameters) extends XSBundle {
   def can_writeback: Bool = exceptionVec.asUInt.orR || singleStep || trigger.canFire
 }
 
-object ExceptionGen {
-  def selectWb2(in0:Valid[RobExceptionInfo], in1:Valid[RobExceptionInfo], p:Parameters):Valid[RobExceptionInfo] = {
-    val valid0 = in0.valid
-    val valid1 = in1.valid
-    val ptr0 = in0.bits.robIdx
-    val ptr1 = in1.bits.robIdx
-    val validVec = Cat(valid1, valid0)
-    val sel = WireInit(true.B)
-    switch(validVec) {
-      is("b01".U) {
-        sel := true.B
-      }
-      is("b10".U) {
-        sel := false.B
-      }
-      is("b11".U) {
-        sel := ptr0 < ptr1
-      }
-    }
-    val res = Mux(sel, in0, in1)
-    res
-  }
-}
-
 class ExceptionGen(wbNum:Int)(implicit p: Parameters) extends XSModule with HasCircularQueuePtrHelper {
   val io = IO(new Bundle {
     val redirect = Input(Valid(new Redirect))
@@ -84,15 +61,21 @@ class ExceptionGen(wbNum:Int)(implicit p: Parameters) extends XSModule with HasC
     a.valid := b.valid && b.bits.has_exception && !(b.bits.robIdx.needFlush(io.redirect))
   })
   private val writebackGroup = writebackAfterSuppress.map(Pipe(_))
-  private val writebackSel = ParallelOperation(writebackGroup, ExceptionGen.selectWb2(_, _, p))
+  private val selector = Module(new SelectPolicy(wbNum, true, false))
+  selector.io.in.zip(writebackGroup).foreach({case(a, b) =>
+    a.valid := b.valid
+    a.bits := b.bits.robIdx
+  })
+  private val oldestSel = selector.io.out
+  private val writebackSel = Mux1H(oldestSel.bits, writebackGroup.map(_.bits))
 
   private val in_enq_valid = VecInit(io.enq.map(e => e.valid && e.bits.has_exception))
   private val realEnqValid = in_enq_valid.asUInt.orR && !io.redirect.valid
   private val enq_valid = RegNext(realEnqValid, false.B)
   private val enq_bits = RegEnable(ParallelPriorityMux(in_enq_valid, io.enq.map(_.bits)), realEnqValid)
 
-  private val s1_out_valid = writebackSel.valid
-  private val s1_out_bits = writebackSel.bits
+  private val s1_out_valid = oldestSel.valid
+  private val s1_out_bits = writebackSel
 
   // s2: compare the input exception with the current one
   // priorities:
