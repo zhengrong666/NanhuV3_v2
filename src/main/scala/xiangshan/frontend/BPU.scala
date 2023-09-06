fa881a1e25937b7402946a52a5905799f71a1e2c
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-09-06 16:00:53 +0800

    Fixed reset vector

--- src/main/scala/xiangshan/frontend/BPU.scala
+++ src/main/scala/xiangshan/frontend/BPU.scala
@@ -238,8 +238,14 @@ class Predictor(parentName:String = "Unknown")(implicit p: Parameters) extends X
   val s1_components_ready_dup, s2_components_ready_dup, s3_components_ready_dup = dup_wire(Bool())
 
   val s0_pc_dup = Wire(Vec(numDup, UInt(VAddrBits.W)))
-  val s0_pc_reg_dup = s0_pc_dup.indices.map(_ => RegInit(io.reset_vector.asTypeOf(UInt(VAddrBits.W))))
-  s0_pc_reg_dup.zip(s0_pc_dup).foreach({case(a, b) => a := b})
+  val s0_pc_reg_dup = s0_pc_dup.indices.map(_ => Reg(UInt(VAddrBits.W)))
+  s0_pc_reg_dup.zip(s0_pc_dup).foreach({case(a, b) =>
+    when(reset.asBool){
+      a := io.reset_vector.asTypeOf(UInt(VAddrBits.W))
+    }.otherwise{
+      a := b
+    }
+  })
   // for debug, would not appear in real RTL
   val s1_pc = RegEnable(s0_pc_dup(0), s0_fire_dup(0))
   val s2_pc = RegEnable(s1_pc, s1_fire_dup(0))
