cb2a68f16437839c6a9c8980a2792078eb66cbed
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-10-01 19:49:20 +0800

    Fixed LS instructions split logics

--- src/main/scala/xiangshan/vector/viwaitqueue/SplitNetwork.scala
+++ src/main/scala/xiangshan/vector/viwaitqueue/SplitNetwork.scala
@@ -2,7 +2,7 @@ package xiangshan.vector.viwaitqueue
 import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
-import xiangshan.{MicroOp, Narrow, Redirect, Widen, XSModule}
+import xiangshan.{MicroOp, Narrow, Redirect, SrcType, Widen, XSModule}
 
 class SplitNetwork(splitNum:Int)(implicit p: Parameters) extends XSModule{
   val io = IO(new Bundle{
@@ -17,49 +17,72 @@ class SplitNetwork(splitNum:Int)(implicit p: Parameters) extends XSModule{
     val sew = in.bits.vCsrInfo.vsew
     val narrow = io.in.bits.ctrl.narrow === Narrow.Narrow
     val narrowToMask = io.in.bits.ctrl.narrow === Narrow.Narrow2
-    val nf = MuxCase(0.U, Seq(
-      (in.bits.ctrl.NField === 0.U) -> 1.U,
-      (in.bits.ctrl.NField === 1.U) -> 2.U,
-      (in.bits.ctrl.NField === 2.U) -> 3.U,
-      (in.bits.ctrl.NField === 3.U) -> 4.U,
-      (in.bits.ctrl.NField === 4.U) -> 5.U,
-      (in.bits.ctrl.NField === 5.U) -> 6.U,
-      (in.bits.ctrl.NField === 6.U) -> 7.U,
-      (in.bits.ctrl.NField === 7.U) -> 8.U,
-    ))
-    def LsShouldRename(idx:UInt):Bool = {
-      MuxCase(false.B, Seq(
-        (sew === 0.U) -> (idx(3, 0) === 0.U),
-        (sew === 1.U) -> (idx(2, 0) === 0.U),
-        (sew === 2.U) -> (idx(1, 0) === 0.U),
-        (sew === 3.U) -> idx(0).asBool,
+    val ctrl = in.bits.ctrl
+    val nf = in.bits.ctrl.NField +& 1.U
+
+    def LsRenameInfo(idx:UInt):(Bool, UInt) = {
+      val vlenBytes = VLEN / 8
+      val vlenShiftBits = log2Ceil(vlenBytes)
+      val idxBits = idx.getWidth
+      val idxDivNf = WireInit(idx)
+      val idxModNf = WireInit(idx)
+      val vecIdxAddend = WireInit(idx)
+      val shouldRename = Wire(Bool())
+      idxDivNf := idx / nf
+      idxModNf := idx % nf
+      shouldRename := MuxCase(false.B, Seq(
+        (sew === 0.U) -> (idxDivNf(vlenShiftBits - 1, 0) === 0.U),
+        (sew === 1.U) -> (idxDivNf(vlenShiftBits - 2, 0) === 0.U),
+        (sew === 2.U) -> (idxDivNf(vlenShiftBits - 3, 0) === 0.U),
+        (sew === 3.U) -> (idxDivNf(vlenShiftBits - 4, 0) === 0.U)
       ))
+      vecIdxAddend := MuxCase(0.U, Seq(
+        (sew === 0.U) -> (idxModNf + idx(idxBits - 1, vlenShiftBits) / nf * nf),
+        (sew === 1.U) -> (idxModNf + idx(idxBits - 1, vlenShiftBits - 1) / nf * nf),
+        (sew === 2.U) -> (idxModNf + idx(idxBits - 1, vlenShiftBits - 2) / nf * nf),
+        (sew === 3.U) -> (idxModNf + idx(idxBits - 1, vlenShiftBits - 3) / nf * nf),
+      ))
+      (shouldRename, vecIdxAddend)
     }
 
     res.zipWithIndex.foreach({case(o , idx) =>
-      val currentnum = in.bits.uopNum - remain + idx.U
+      val currentnum = Wire(UInt(7.W))
+      currentnum := in.bits.uopNum - remain + idx.U
       o.valid := io.in.valid && (idx.U < remain)
       o.bits := in.bits
       o.bits.uopNum := in.bits.uopNum
       o.bits.uopIdx := currentnum
       o.bits.isTail := currentnum >= vl //Only VLS need this
       o.bits.isPrestart := currentnum < vstart //Only VLS need this
-      o.bits.canRename := true.B
-      when(o.bits.ctrl.isSeg){
-        o.bits.canRename := currentnum <= in.bits.ctrl.NField
-      }.elsewhen(in.bits.ctrl.isVLS){
-        o.bits.canRename := LsShouldRename(currentnum)
+
+      when(in.bits.ctrl.isVLS){
+        val (doRn, addend) = LsRenameInfo(currentnum)
+        o.bits.canRename := doRn
+        o.bits.ctrl.ldest := ctrl.ldest + addend
+        o.bits.ctrl.lsrc(0) := ctrl.lsrc(0)
+        o.bits.ctrl.lsrc(1) := Mux(ctrl.srcType(1) === SrcType.vec, ctrl.lsrc(1) + addend, ctrl.lsrc(1))
+        o.bits.ctrl.lsrc(2) := ctrl.lsrc(2) + addend
       }.elsewhen(narrow){
+        val addend = currentnum(6, 1)
         o.bits.canRename := currentnum(0).asBool
+        o.bits.ctrl.ldest := ctrl.ldest + addend
+        o.bits.ctrl.lsrc(0) := Mux(ctrl.srcType(0) === SrcType.vec, ctrl.lsrc(0) + addend, ctrl.lsrc(1))
+        o.bits.ctrl.lsrc(1) := ctrl.lsrc(1) + addend
+        o.bits.ctrl.lsrc(2) := ctrl.lsrc(2) + addend
       }.elsewhen(narrowToMask){
+        val addend = currentnum
         o.bits.canRename := currentnum === 0.U
+        o.bits.ctrl.ldest := ctrl.ldest
+        o.bits.ctrl.lsrc(0) := Mux(ctrl.srcType(0) === SrcType.vec, ctrl.lsrc(0) + addend, ctrl.lsrc(0))
+        o.bits.ctrl.lsrc(1) := Mux(ctrl.srcType(1) === SrcType.vec, ctrl.lsrc(1) + addend, ctrl.lsrc(1))
+        o.bits.ctrl.lsrc(2) := Mux(ctrl.srcType(2) === SrcType.vec, ctrl.lsrc(2) + addend, ctrl.lsrc(2))
+      }.otherwise{
+        o.bits.canRename := true.B
+        o.bits.pdest := in.bits.pdest + idx.U
+        o.bits.ctrl.lsrc(0) := Mux(ctrl.srcType(0) === SrcType.vec, ctrl.lsrc(0) + idx.U, ctrl.lsrc(0))
+        o.bits.ctrl.lsrc(1) := Mux(ctrl.srcType(1) === SrcType.vec, ctrl.lsrc(1) + idx.U, ctrl.lsrc(1))
+        o.bits.ctrl.lsrc(2) := Mux(ctrl.srcType(2) === SrcType.vec, ctrl.lsrc(2) + idx.U, ctrl.lsrc(2))
       }
-      val vlsNum = currentnum >> (4.U - sew)
-      val tempnum = Mux(in.bits.ctrl.isSeg, currentnum % nf,
-        Mux(in.bits.ctrl.isVLS, vlsNum, currentnum))
-      o.bits.ctrl.lsrc(0) := in.bits.ctrl.lsrc(0) + tempnum
-      o.bits.ctrl.lsrc(1) := in.bits.ctrl.lsrc(1) + tempnum
-      o.bits.ctrl.lsrc(2) := in.bits.ctrl.lsrc(2) + tempnum
     })
     res
   }
