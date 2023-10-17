0219608a821466c17dc433c267252c9154f88ce0
gmx <guanmingxing@bosc.ac.cn>
2023-10-17 14:56:19 +0800

    difftest signals update

--- src/main/scala/xiangshan/backend/execute/fu/csr/CSR.scala
+++ src/main/scala/xiangshan/backend/execute/fu/csr/CSR.scala
@@ -1326,12 +1326,12 @@ class CSR(implicit p: Parameters) extends FUWithRedirect
     val difftest = DifftestModule(new DiffVecCSRState, delay = 1)
     difftest.coreid := csrio.hartId
     difftest.vlenb := vlenb
-    difftest.vxsat := Mux(csrio.vcsr.robWb.vxsat.valid, csrio.vcsr.robWb.vxsat.bits, Cat(0.U((XLEN-1).W), vcsr(0)))
+    difftest.vxsat := RegNext(Mux(csrio.vcsr.robWb.vxsat.valid, csrio.vcsr.robWb.vxsat.bits, Cat(0.U((XLEN-1).W), vcsr(0))))
     difftest.vxrm := Cat(0.U((XLEN-3).W), vcsr(2, 1))
-    difftest.vcsr := Mux(csrio.vcsr.robWb.vxsat.valid, Cat(vcsr(XLEN - 1, 1), csrio.vcsr.robWb.vxsat.bits(0)), vcsr)
+    difftest.vcsr := RegNext(Mux(csrio.vcsr.robWb.vxsat.valid, Cat(vcsr(XLEN - 1, 1), csrio.vcsr.robWb.vxsat.bits(0)), vcsr))
     difftest.vtype := csrio.vcsr.vtype.debug_vtype
     difftest.vl := csrio.vcsr.vtype.debug_vl
-    difftest.vstart := Mux(csrio.vcsr.robWb.vstart.valid, csrio.vcsr.robWb.vstart.bits, vstart)
+    difftest.vstart := RegNext(Mux(csrio.vcsr.robWb.vstart.valid, csrio.vcsr.robWb.vstart.bits, vstart))
   }
 }
 
