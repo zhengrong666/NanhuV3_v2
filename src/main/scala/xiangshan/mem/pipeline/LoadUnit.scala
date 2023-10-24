868194c381151ab159f7fe8418a3e2b0e07677ef
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-10-25 00:21:03 +0800

    Optimized lpv cancel timing

--- src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
+++ src/main/scala/xiangshan/mem/pipeline/LoadUnit.scala
@@ -296,6 +296,7 @@ class LoadUnit_S2(implicit p: Parameters) extends XSModule with HasLoadHelper wi
     val static_pm = Input(Valid(Bool())) // valid for static, bits for mmio
     val s2_can_replay_from_fetch = Output(Bool()) // dirty code
     val loadDataFromDcache = Output(new LoadDataFromDcacheBundle)
+    val lpvCancel = Output(Bool())
   })
 
   val pmp = WireInit(io.pmpResp)
@@ -434,7 +435,7 @@ class LoadUnit_S2(implicit p: Parameters) extends XSModule with HasLoadHelper wi
   // if ld-ld violation is detected, replay from this inst from fetch
   val debug_ldldVioReplay = s2_ldld_violation && !s2_mmio && !s2_is_prefetch && !s2_tlb_miss
   // io.out.bits.uop.ctrl.replayInst := false.B
-
+  io.lpvCancel := io.in.valid && (s2_tlb_miss || s2_mmio || io.lsq.dataInvalid || s2_cache_miss || s2_cache_replay)
   io.out.bits.mmio := s2_mmio
   io.out.bits.uop.ctrl.flushPipe := false.B  ///flushPipe logic is useless
   io.out.bits.uop.cf.exceptionVec := s2_exception_vec // cache error not included
@@ -871,7 +872,7 @@ class LoadUnit(implicit p: Parameters) extends XSModule with HasLoadHelper with 
 
   private val s1_cancel = RegInit(false.B)
   s1_cancel := load_s1.io.in.valid && (!load_s1.io.out.valid)
-  io.cancel := s1_cancel || (load_s2.io.in.valid && (!hitLoadOut.valid))
+  io.cancel := s1_cancel || load_s2.io.lpvCancel
 
   val perfEvents = Seq(
     ("load_s0_in_fire         ", load_s0.io.in.fire                                                                                                              ),
