ccb2064ed1c88bbc4bf94154c9633f019a4a8fdc
gmx <guanmingxing@bosc.ac.cn>
2023-11-12 10:48:05 +0800

    regression script update

--- regression/regression.py
+++ regression/regression.py
@@ -22,7 +22,7 @@ def main():
 	elif cfg['config']['sim_mode'] == 'all':
 		test_list = cfg['case']['list']
 	print(test_list)
-	#multi_task.multi_task_run(5, cfg, test_list)
+	multi_task.multi_task_run(5, cfg, test_list)
 	record.sim_result_statistic(cfg, test_list)
 	
 
