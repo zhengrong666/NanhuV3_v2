8a04c1869e0206a9454b65c41dbe964944f6d2fb
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-11-21 18:36:10 +0800

    [Writeback] When an inst may be flushed, block it's writebacked.

--- src/main/scala/xiangshan/backend/rob/WbStateArray.scala
+++ src/main/scala/xiangshan/backend/rob/WbStateArray.scala
@@ -7,41 +7,63 @@ class WbReq(implicit p: Parameters) extends XSBundle {
   val robIdx = new RobPtr
   val data = Bool()
 }
-class WbStateArray(wbNum:Int, redirectNum:Int)(implicit p: Parameters) extends XSModule{
+class WbStateArray(enqNum:Int, wbNum:Int, redirectNum:Int)(implicit p: Parameters) extends XSModule{
   val io = IO(new Bundle {
-    val updateIn = Input(Vec(wbNum, Valid(new WbReq)))
+    val enqIn = Input(Vec(enqNum, Valid(new WbReq)))
+    val wbIn = Input(Vec(wbNum, Valid(new WbReq)))
     val redirectIn = Input(Vec(redirectNum, Valid(new WbReq)))
     val out = Output(Vec(RobSize, Bool()))
   })
-  private val in = io.updateIn
-  private val inr = io.redirectIn
+  private val enq = io.enqIn
+  private val wb = io.wbIn
+  private val rdc = io.redirectIn
   private val writebacked = RegInit(VecInit(Seq.fill(RobSize)(false.B)))
-  for((wb, idx) <- writebacked.zipWithIndex) {
-    val inSel = in.map(r => r.valid && r.bits.robIdx.value === idx.U)
-    val inData = Mux1H(inSel, in.map(_.bits.data))
-    val inHit = Cat(inSel).orR
-    val inrSel = inr.map(r => r.valid && r.bits.robIdx.value === idx.U)
-    val inrData = Mux1H(inrSel, inr.map(_.bits.data))
-    val inrHit = Cat(inrSel).orR
-    wb := Mux(inrHit, inrData, Mux(inHit, inData, wb))
+  private val mayBeFlushed = RegInit(VecInit(Seq.fill(RobSize)(false.B)))
+  for(i <- writebacked.indices) {
+    val enqSel = enq.map(r => r.valid && r.bits.robIdx.value === i.U)
+    val enqData = Mux1H(enqSel, enq.map(_.bits.data))
+    val wbSel = wb.map(r => r.valid && r.bits.robIdx.value === i.U)
+    val wbData = Mux1H(wbSel, wb.map(_.bits.data))
+    val rdcSel = rdc.map(r => r.valid && r.bits.robIdx.value === i.U)
+    val rdcData = Mux1H(rdcSel, rdc.map(_.bits.data))
+    val enqHit = Cat(enqSel).orR
+    val wbHit = Cat(wbSel).orR
+    val rdcHit = Cat(rdcSel).orR
+    when(enqHit) {
+      writebacked(i) := enqData
+      mayBeFlushed(i) := false.B
+    }.elsewhen(rdcHit) {
+      writebacked(i) := rdcData
+      mayBeFlushed(i) := true.B
+    }.elsewhen(wbHit) {
+      writebacked(i) := wbData & !mayBeFlushed(i)
+    }
   }
 
   io.out.zip(writebacked).foreach({case(a, b) => a := b})
 
-  private var upidx = 0
-  private var rdidx = 0
+  private var enqIdx = 0
+  private var wbIdx = 0
+  private var rdcIdx = 0
 
-  def update(v:Bool, addr:RobPtr, data:Bool):Unit = {
-    this.io.updateIn(upidx).valid := v
-    this.io.updateIn(upidx).bits.robIdx := addr
-    this.io.updateIn(upidx).bits.data := data
-    upidx = upidx + 1
+  def enqueue(v: Bool, addr: RobPtr, data: Bool): Unit = {
+    this.io.enqIn(enqIdx).valid := v
+    this.io.enqIn(enqIdx).bits.robIdx := addr
+    this.io.enqIn(enqIdx).bits.data := data
+    enqIdx = enqIdx + 1
+  }
+
+  def writeback(v:Bool, addr:RobPtr, data:Bool):Unit = {
+    this.io.wbIn(wbIdx).valid := v
+    this.io.wbIn(wbIdx).bits.robIdx := addr
+    this.io.wbIn(wbIdx).bits.data := data
+    wbIdx = wbIdx + 1
   }
 
   def redirect(v:Bool, addr:RobPtr, data:Bool):Unit = {
-    this.io.redirectIn(rdidx).valid := v
-    this.io.redirectIn(rdidx).bits.robIdx := addr
-    this.io.redirectIn(rdidx).bits.data := data
-    rdidx = rdidx + 1
+    this.io.redirectIn(rdcIdx).valid := v
+    this.io.redirectIn(rdcIdx).bits.robIdx := addr
+    this.io.redirectIn(rdcIdx).bits.data := data
+    rdcIdx = rdcIdx + 1
   }
 }
