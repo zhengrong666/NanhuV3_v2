9f9b02b73b3cf5ba2d77f222edb4f44f8696047b
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-11-24 15:39:46 +0800

    [DMA] DMA fully support for burst transaction

--- src/main/scala/system/SoC.scala
+++ src/main/scala/system/SoC.scala
@@ -121,7 +121,7 @@ trait HaveSlaveAXI4Port {
     TLBuffer() :=
     AXI2TL(16, 16, soc.hasMbist, soc.hasShareBus) :=
     AXI4Buffer() :=
-    AXI4Fragmenter() :=
+    AXI2TLFragmenter() :=
     AXI4Buffer() :=
     l3FrontendAXI4Node
   errorDevice.node := l3_xbar
