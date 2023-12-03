27699dd4a613f14c2f343cd73f8e5da0aa107ab3
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-12-03 14:07:42 +0800

    [Prefetch] Removed SPP

--- src/main/scala/top/Configs.scala
+++ src/main/scala/top/Configs.scala
@@ -258,7 +258,7 @@ class WithNKBL2
         enablePerf = false,
         hasMbist = p.hasMbist,
         hasShareBus = p.hasShareBus,
-        prefetch = Some(coupledL2.prefetch.HyperPrefetchParams()),
+        prefetch = Some(coupledL2.prefetch.PrefetchReceiverParams()),
         /*
         del L2 prefetche recv option, move into: prefetch =  PrefetchReceiverParams
         prefetch options:
