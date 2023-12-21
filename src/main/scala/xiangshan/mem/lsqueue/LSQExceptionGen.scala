093a9d9ebf5ef53343e3033f71f46395c1832252
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-12-21 15:01:52 +0800

    [LSQ] Record the younger exception info with LSQ ptr, not ROB ptr.

--- src/main/scala/xiangshan/mem/lsqueue/LSQExceptionGen.scala
+++ src/main/scala/xiangshan/mem/lsqueue/LSQExceptionGen.scala
@@ -3,19 +3,22 @@ import chisel3._
 import chisel3.util._
 import org.chipsalliance.cde.config.Parameters
 import xiangshan.backend.execute.fu.{FuConfig, FuConfigs}
-import xiangshan.{ExceptionNO, ExceptionVec, Redirect, XSModule}
+import xiangshan.{ExceptionNO, ExceptionVec, FuType, Redirect, XSModule}
 import xiangshan.backend.rob.RobPtr
 import xiangshan.cache.DCacheBundle
+import xiangshan.mem.{LqPtr, SqPtr}
 import xs.utils.ParallelOperation
 
 class LSQExceptionInfo (implicit p: Parameters)  extends DCacheBundle{
   val eVec = ExceptionVec()
   val robIdx = new RobPtr
+  val sqIdx = new SqPtr
+  val lqIdx = new LqPtr
   val vaddr = UInt(VAddrBits.W)
   val segIdx = UInt(log2Ceil(VLEN + 1).W)
 }
 
-class ExceptionSelector(inNum:Int)(implicit p: Parameters) extends XSModule {
+class ExceptionSelector(inNum:Int, isLq:Boolean)(implicit p: Parameters) extends XSModule {
   val io = IO(new Bundle {
     val in = Input(Vec(inNum, Valid(new LSQExceptionInfo)))
     val out = Output(Valid(new LSQExceptionInfo))
@@ -24,7 +27,11 @@ class ExceptionSelector(inNum:Int)(implicit p: Parameters) extends XSModule {
   private def GetOldest(in0: Valid[LSQExceptionInfo], in1: Valid[LSQExceptionInfo]):Valid[LSQExceptionInfo] = {
     val res = Wire(Valid(new LSQExceptionInfo))
     res.valid := in0.valid | in1.valid
-    val in0IsOlder = (in0.bits.robIdx < in1.bits.robIdx) || (in0.bits.robIdx === in1.bits.robIdx && in0.bits.segIdx < in1.bits.segIdx)
+    val in0IsOlder = if(isLq) {
+      in0.bits.lqIdx < in1.bits.lqIdx
+    } else {
+      in0.bits.sqIdx < in1.bits.sqIdx
+    }
     val sel = Cat(in1.valid, in0.valid)
     when(sel === 1.U) {
       res.bits := in0.bits
@@ -50,6 +57,7 @@ class LSQExceptionGen(wbInNum:Int, fuCfg:FuConfig)(implicit p: Parameters) exten
     val redirect = Input(Valid(new Redirect))
   })
   private val exceptionInfo = RegInit(0.U.asTypeOf(Valid(new LSQExceptionInfo())))
+  private val isLq = fuCfg.fuType.litValue == FuType.ldu.litValue
 
   private val s1EcptReg = io.in.map(i => {
     val res = Wire(Valid(new LSQExceptionInfo))
@@ -59,7 +67,7 @@ class LSQExceptionGen(wbInNum:Int, fuCfg:FuConfig)(implicit p: Parameters) exten
     res
   })
 
-  private val selector = Module(new ExceptionSelector(wbInNum + 1))
+  private val selector = Module(new ExceptionSelector(wbInNum + 1, isLq))
   selector.io.in.zip(s1EcptReg :+ exceptionInfo).foreach({case(a, b) =>
     a.valid := b.valid && !b.bits.robIdx.needFlush(io.redirect)
     a.bits := b.bits
