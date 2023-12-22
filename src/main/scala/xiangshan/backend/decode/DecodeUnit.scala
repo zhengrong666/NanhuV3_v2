fc852395ceebbe9a0b0dbac88c37ae1cead536a9
梁森 Liang Sen <liangsen20z@ict.ac.cn>
2023-12-22 17:27:58 +0800

    [FloatPoint] Raise illegal inst exception when rm is not supported.

--- src/main/scala/xiangshan/backend/decode/DecodeUnit.scala
+++ src/main/scala/xiangshan/backend/decode/DecodeUnit.scala
@@ -1079,8 +1079,8 @@ class DecodeUnit(implicit p: Parameters) extends XSModule with DecodeUnitConstan
   val extEn = io.csrCtrl.extEn
 
   val isFp = FuType.floatingTypes.map(_ === cs.fuType).reduce(_ | _)
-  val illegalFrm = io.csrCtrl.frm > 4.U
-  val illegalFp = isFp && ((illegalFrm && cs.fpu.rm === 7.U) || !extEn.fp)
+  val illegalRm = (io.csrCtrl.frm > 4.U && cs.fpu.rm === 7.U) || (cs.fpu.rm > 4.U && cs.fpu.rm < 7.U)
+  val illegalFp = isFp && (illegalRm || !extEn.fp)
 
   val illegalVec = (isVector || isVtype) && !extEn.vec
 
